4c8089f1990a0712b9e4169d69630fe4c429bbf4
macroguru <99953873+macroguru@users.noreply.github.com>
2023-04-19 02:22:36 +0300

    Add files via upload

--- ValuteMOD.py
+++ ValuteMOD.py
@@ -6,12 +6,11 @@ from .. import loader, utils
 class ValuteMod(loader.Module):
     """Конвертер валют. Edited by @weeldag"""
 
-    strings = {"name": "ValuteMOD"}
+    strings = {"name": "Valute"}
 
     async def valcmd(self, message):
         """<сумма> <валюта> - получить курс"""
         state = utils.get_args_raw(message)
-        await utils.answer(message, "<b>Данные получены</b>")
         chat = "@exchange_rates_vsk_bot"
         async with message.client.conversation(chat) as conv:
             try:
@@ -23,17 +22,22 @@ class ValuteMod(loader.Module):
                 )
                 bot_response = await response
                 bot_response_text = bot_response.message.message
-                bot_response_text = bot_response_text.replace("#GloryToUA", "")
-                bot_response_text = bot_response_text.replace("https://200rf.com/", "")
-                bot_response_text = bot_response_text.replace("======", "")
-                bot_response_text = bot_response_text.replace("🇷🇺", "<emoji document_id=6323139226418284334>🇷🇺</emoji> ")
-                bot_response_text = bot_response_text.replace("🇧🇾", "<emoji document_id=6323582299539506720>🇧🇾</emoji> ")
-                bot_response_text = bot_response_text.replace("🇪🇺", "<emoji document_id=6323217102765295143>🇪🇺</emoji> ")
-                bot_response_text = bot_response_text.replace("🇺🇦", "<emoji document_id=6323289850921354919>🇺🇦</emoji> ")
-                bot_response_text = bot_response_text.replace("🇺🇸", "<emoji document_id=6323374027985389586>🇺🇸</emoji> ")
-                bot_response_text = bot_response_text.replace("🇰🇿", "<emoji document_id=6323135275048371614>🇰🇿</emoji> ")
-                bot_response_text = bot_response_text.replace("🇹🇷", "<emoji document_id=6321003171678259486>🇹🇷</emoji> ")
-                bot_response_text = bot_response_text.replace("🇺🇿", "<emoji document_id=6323430017179059570>🇺🇿</emoji> ")
+                replacements = {
+                    "#GloryToUA": "",
+                    "https://200rf.com/": "",
+                    "======": "",
+                    "🇷🇺": "<emoji document_id=6323139226418284334>🇷🇺</emoji> ",
+                    "🇧🇾": "<emoji document_id=6323582299539506720>🇧🇾</emoji> ",
+                    "🇪🇺": "<emoji document_id=6323217102765295143>🇪🇺</emoji> ",
+                    "🇺🇦": "<emoji document_id=6323289850921354919>🇺🇦</emoji> ",
+                    "🇺🇸": "<emoji document_id=6323374027985389586>🇺🇸</emoji> ",
+                    "🇰🇿": "<emoji document_id=6323135275048371614>🇰🇿</emoji> ",
+                    "🇹🇷": "<emoji document_id=6321003171678259486>🇹🇷</emoji> ",
+                    "🇺🇿": "<emoji document_id=6323430017179059570>🇺🇿</emoji> ",
+                }
+                for key, value in replacements.items():
+                    bot_response_text = bot_response_text.replace(key, value)
+
                 await bot_send_message.delete()
                 await utils.answer(message, f"<b>{bot_response_text.strip()}</b>")
                 await bot_response.delete()
